76eeb2cdb2a09840478273a52e72402ff7cf353d
yusikerem <kerem.yky@gmail.com>
2023-02-06 17:14:30 +0300

    import tailwind, edit tailwind config, create components and header

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    fontFamily: {
+      mono: ["Space Mono"],
+      sans: ["Work Sans"],
+    },
+    fontSize: {
+      sm: "12px",
+      base: "16px",
+      lg: "22px",
+      xl: "28px",
+      "2xl": "38px",
+      "3xl": "51px",
+      "4xl": "67px",
+    },
+    backgroundColor: {
+      primary: "#2B2B2B",
+      secondary: "#3B3B3B",
+      purple: "#A259FF",
+    },
+    fill: {
+      purple: "#A259FF",
+    },
+    lineHeight: {
+      normal: 1.4,
+    },
+    borderColor: {
+      purple: "#A259FF",
+    },
+    textColor: {
+      primary: "#858584",
+      white: "#ffff",
+      purple: "#A259FF",
+    },
+    extend: {
+      backgroundImage: {
+        gradient_primary: "linear-gradient(to right bottom, #a259ff,#686cfa)",
+        gradient_secondary: "linear-gradient(to right bottom, #a259ff,#ff6251)",
+      },
+    },
+  },
+  plugins: [],
+};
